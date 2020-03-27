5ccc89c6deb4d00158aaabba062c33cc1289c5ba
Adam Bungart <abungart@gmail.com>
2020-03-27 16:30:42 -0500

    Buttons connected

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,26 @@ $(document).ready(init);
 
 function init() {
   console.log("In init");
+  $(".btn_equals").on("click", compute);
+  $(".num").on("click", numEntry);
+  $(".operator").on("click", operatorEntry);
+  $(".btn_clear").on("click", clearField);
+}
+
+function clearField() {
+  console.log("In clear");
+}
+
+function compute() {
+  console.log("Calculating");
+}
+
+function numEntry() {
+  let thisNumber = $(this).data("index");
+  console.log(thisNumber);
+}
+
+function operatorEntry() {
+  let operator = $(this).data("index");
+  console.log(operator);
 }
